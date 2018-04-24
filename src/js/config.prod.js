c4e7cbc2718139398eb1262a36c666e2c2c00a5b
Md. Abu Taher <entrptaher@users.noreply.github.com>
2018-04-24 21:02:49 +0600

    Add instruction data

--- src/js/config.prod.js
+++ src/js/config.prod.js
@@ -1,6 +1,7 @@
+// check the config.js for example
 window.globalSettings = {
-  domain: "",
-  queryURL: "",
-  homeURL: "",
-  loginUrl: ""
+  domain: "", // ie: example.com or localhost - to check various cors, cookie, session info
+  queryURL: "", // http://example.com/graphql - api to request various data
+  homeURL: "", // http://example.com - url to use as base data
+  loginUrl: "" // http://example.com/popup/login - url used for login popup
 };
